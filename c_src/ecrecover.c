53b4d36a29ba0f4b2d859c78b8773c71e322c78b
Sean Hinde <sean.hinde@mac.com>
2023-01-12 14:52:32 +0100

    Switched to C based nif

---
+++ c_src/ecrecover.c
@@ -0,0 +1,102 @@
+#include "erl_nif.h"
+#include "secp256k1_recovery.h"
+
+
+static secp256k1_context *ctx = NULL;
+
+static ERL_NIF_TERM error_result(ErlNifEnv* env, char* error_msg)
+{
+    return enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_string(env, error_msg, ERL_NIF_LATIN1));
+}
+
+static ERL_NIF_TERM ok_result(ErlNifEnv* env, ERL_NIF_TERM *r)
+{
+    return enif_make_tuple2(env, enif_make_atom(env, "ok"), *r);
+}
+
+static ERL_NIF_TERM
+recover(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+  ERL_NIF_TERM r;
+	ErlNifBinary message, csignature;
+	int result;
+    int compressed = SECP256K1_EC_UNCOMPRESSED;
+	size_t pubkeylen = 65;
+	int recid;
+    unsigned char* finished_recpubkey_buf;
+    secp256k1_ecdsa_recoverable_signature signature;
+    secp256k1_pubkey recpubkey;
+	
+	if (!enif_inspect_binary(env, argv[0], &message)) {
+       return enif_make_badarg(env);
+    }
+
+	if (!enif_inspect_binary(env, argv[1], &csignature)) {
+       return enif_make_badarg(env);
+    }
+
+    if (!enif_get_int(env, argv[2], &recid)) {
+		return error_result(env, "Recovery id invalid 0-3");
+	}
+
+    if (recid < 0 || recid > 3) {
+		error_result(env, "Recovery id invalid 0-3x");
+	}
+printf("xx1\r\n");
+    result = secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &signature, csignature.data, recid);
+printf("xx2\r\n");
+	if (!result) {
+		return error_result(env, "ecdsa_signature_parse_compact returned 0");
+	}
+
+	// Now do ECDSA recovery
+	result = secp256k1_ecdsa_recover(ctx, &recpubkey, &signature, message.data);
+
+	if (!result) {
+	    return error_result(env, "ecdsa recovery problem");
+	}
+
+	// Now serialize recpubkey based on the compression flag
+	finished_recpubkey_buf = enif_make_new_binary(env, pubkeylen, &r);
+
+    printf("pubker[0] = %d\r\n", recpubkey.data[0]);
+
+
+	result = secp256k1_ec_pubkey_serialize(ctx, finished_recpubkey_buf,
+			&pubkeylen, &recpubkey, compressed);
+
+	if (!result) {
+		return error_result(env, "ecdsa pubkey serialize error");
+	}
+
+    return ok_result(env, &r);
+
+}
+
+
+static int
+load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
+{
+    ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+    return 0;
+}
+
+static int
+upgrade(ErlNifEnv* env, void** priv, void** old_priv, ERL_NIF_TERM load_info)
+{
+    return 0;
+}
+
+static void
+unload(ErlNifEnv* env, void* priv)
+{
+	secp256k1_context_destroy(ctx);
+    return;
+}
+
+static ErlNifFunc nif_funcs[] =
+  {
+   {"recover", 3, recover}
+  };
+
+ERL_NIF_INIT(ecrecover, nif_funcs, &load, NULL, NULL, &unload);
--- src/ecrecover.erl
+++ src/ecrecover.erl
@@ -1,7 +1,9 @@
 -module(ecrecover).
 
+%% https://github.com/mbrix/libsecp256k1
+
 %% API
--export([recover/2]).
+-export([recover/2, recover/3]).
 
 %% NIF
 -export([load/0]).
@@ -9,16 +11,17 @@
 
 %%=============================================================================
 %% NIF API
+%% GoodMsg1 = <<71,23,50,133,168,215,52,30,94,151,47,198,119,40,99,132,248,2,248,239,66,165,236,95,3,187,250,37,76,176,31,173>>.
+%% GoodSig1_v = <<27,101,10,207,157,63,95,10,44,121,151,118,161,37,67,85,213,244,6,23,98,162,55,57,106,153,160,224,227,252,43,205,103,41,81,74,13,172,178,230,35,172,74,189,21,124,177,129,99,255,148,34,128,219,77,92,170,214,109,223,148,27,161,46,3>>.
+%% ecrecover:recover(<<71,23,50,133,168,215,52,30,94,151,47,198,119,40,99,132,248,2,248,239,66,165,236,95,3,187,250,37,76,176,31,173>>, <<27,101,10,207,157,63,95,10,44,121,151,118,161,37,67,85,213,244,6,23,98,162,55,57,106,153,160,224,227,252,43,205,103,41,81,74,13,172,178,230,35,172,74,189,21,124,177,129,99,255,148,34,128,219,77,92,170,214,109,223,148,27,161,46,3>>).
+%% 
 
 load() ->
-    % Prefer locally built NIF (good for testing and exotic platforms) over
-    % prebuilt binaries.
-    case load_local_nif() of
-        ok ->
-            ok;
-        {error, _} ->
-            load_prebuilt_nif()
-    end.
+    EbinDir = filename:dirname(code:which(?MODULE)),
+    AppDir = filename:dirname(EbinDir),
+    PrivDir = filename:join(AppDir, "priv"),
+    SoName = filename:join(PrivDir, atom_to_list(?MODULE)),
+    erlang:load_nif(SoName, 0).
 
 not_loaded(Line) ->
     erlang:nif_error({error, {not_loaded, [{module, ?MODULE}, {line, Line}]}}).
@@ -26,36 +29,22 @@ not_loaded(Line) ->
 %%=============================================================================
 %% External API
 
--spec recover(<<_:(32*8)>>, <<_:(65*8)>>) -> <<_:(32*8)>>.
-recover(<<_:32/binary>> = Hash, <<_:65/binary>> = Sig) ->
-    Input = <<Hash/binary, 0:(8*31), Sig/binary>>,
-    case recover_(Input) of
-        {ok, []} ->
-            <<0:256>>;
-        {ok, Res} ->
-            erlang:list_to_binary(Res);
-        _Err ->
+recover(Hash, <<V, Sig:64/binary>>) when V == 27; V == 28 ->
+    RecId = V - 27,
+    case recover(Hash, Sig, RecId) of
+        {ok, <<4, XY:64/binary>>} ->
+            <<_:12/bytes, ShortPub:20/bytes>> = keccak256(XY),
+            <<0:96, ShortPub/binary>>;
+        {error, _} ->
             <<0:256>>
-    end.
+        end.
+
+-spec recover(<<_:(32*8)>>, <<_:(65*8)>>, integer()) -> <<_:(32*8)>>.
+recover(_Hash, _Sig, _RecId) ->
+    not_loaded(?LINE).
 
 %%=============================================================================
 %% Internal Functions
 
-load_local_nif() ->
-    EbinDir = filename:dirname(code:which(?MODULE)),
-    AppDir = filename:dirname(EbinDir),
-    PrivDir = filename:join(AppDir, "priv"),
-    SoName = filename:join(PrivDir, atom_to_list(?MODULE)),
-    erlang:load_nif(SoName, 0).
-
-load_prebuilt_nif() ->
-    case code:priv_dir(ecrecoverprebuilt) of
-        {error, _} ->
-            {error, prebuilt_priv_dir_not_found};
-        PrivDir ->
-            SoName = filename:join(PrivDir, atom_to_list(?MODULE)),
-            erlang:load_nif(SoName, 0)
-    end.
-
-recover_(_Input) ->
-    not_loaded(?LINE).
+keccak256(Bin) ->
+    sha3:hash(256, Bin).
